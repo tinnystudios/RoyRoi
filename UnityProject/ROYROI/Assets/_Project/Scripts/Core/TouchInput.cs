9893b9e8a2b9ded86e338a4e3fb59f437a7a79f9
Tin <tinnystudios@gmail.com>
2018-06-08 12:19:02 +1000

    Added scroll area for Profile & Settings page. Allowing us to add as many info as we need and still work on different screen res

--- UnityProject/ROYROI/Assets/_Project/Scripts/Core/TouchInput.cs
+++ UnityProject/ROYROI/Assets/_Project/Scripts/Core/TouchInput.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 //#TODO Create the touch input module currently SideBar is generatin delta & pressed which should belong to touchInput
