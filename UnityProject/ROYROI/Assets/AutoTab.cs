3538b412ded97a45587127a9ecb8dcb0d56896cb
Tin <tinnystudios@gmail.com>
2018-06-08 15:36:54 +1000

    Basic Payment Page. (You can't add a card yet)

---
+++ UnityProject/ROYROI/Assets/AutoTab.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AutoTab : MonoBehaviour {
+
+    private InputField m_InputField;
+    public Selectable m_NextSelectable;
+    
+    private void Awake()
+    {
+        m_InputField = GetComponent<InputField>();
+        m_InputField.onValueChanged.AddListener(OnValueCompleted);
+    }
+
+    private void OnDestroy()
+    {
+        m_InputField.onValueChanged.RemoveListener(OnValueCompleted);
+    }
+
+    public void OnValueCompleted(string value)
+    {
+        if (value.Length >= m_InputField.characterLimit)
+        {
+            //Go to next tab
+            m_NextSelectable.Select();
+        }
+    }
+}
