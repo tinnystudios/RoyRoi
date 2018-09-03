c2911f9e41119070ab7ec189e698a2e79f156d3e
tinny <tinnystudios@gmail.com>
2018-09-04 08:39:49 +1000

    Logo - Need Icon

---
+++ UnityProject/ROYROI/Assets/Logo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Logo : MonoBehaviour
+{
+    public float m_Delay = 2;
+
+    public void LogoCompleted()
+    {
+        StartCoroutine(LoadMainApp());
+    }
+
+    IEnumerator LoadMainApp()
+    {
+        yield return new WaitForSeconds(m_Delay);
+        SceneManager.LoadScene(1);
+    }
+}
